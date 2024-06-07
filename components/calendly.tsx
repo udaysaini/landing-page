3a6930c7d75e646182b783fea28d30aa222c2219
Uday Saini <uday10saini@gmail.com>
2024-06-07 16:44:57 -0400

    Added Calendly and Footer sections

--- components/calendly.tsx
+++ components/calendly.tsx
@@ -1,8 +1,17 @@
-export default function Calendly () {
+import { InlineWidget } from "react-calendly"
+import { Separator } from "./ui/separator"
+
+export const Calendly = () => {
     return (
-        <div>
-            Calendly
+        <div className="text-center" id='calendly'>
+            <h1 className="pb-4 font-bold tracking-tight text-4xl lg:text-4xl">Ready to Transform Your Content? <br /> Book a Free Editing Consultation Below!</h1>
+            <div className="flex items-center justify-center">
+                <Separator className="mt-3 bg-slate-100/20 h-0.5 w-40" />
+            </div>
+            <InlineWidget url="https://calendly.com/uday10saini/30min" styles={{ "height": "1000px", "borderRadius": "5px" }} />
+
         </div>
     )
 }
 
+export default Calendly;
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,44 @@
+import XIcon from '@mui/icons-material/X';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Link from 'next/link';
+
 export const Footer = () => {
+    const links = [
+        {
+            href: 'https://twitter.com/uday10saini',
+            icon: <XIcon className='h-8 w-8' />
+        },
+        {
+            href: 'https://www.instagram.com/udaysaini11/',
+            icon: <InstagramIcon className='h-9 w-9' />
+        },
+        {
+            href: 'https://www.youtube.com/@geekyathletetech',
+            icon: <YouTubeIcon className='h-10 w-10' />
+        },
+        {
+            href: 'mailto:uday10saini@gmail.com',
+            icon: <EmailIcon className='h-8 w-8' />
+        }
+    ]
     return (
-        <div>
-            Footer
+        <div className='text-center pb-8'>
+            <div className='flex items-center justify-center space-x-4'>
+
+                {links.map((link, index) => (
+                    <Link href={link.href} key={index} className='hover:opacity-45 transition duration-300'>
+                        {link.icon}
+                    </Link>
+
+                ))}
+
+            </div>
+            <p className='text-sm text-slate-100/50'>uday10saini@gmail.com</p>
+            <p className='text-sm text-slate-100/50'>© Uday Saini. All rights reserved.</p>
         </div>
     )
-};
+}
 
 export default Footer;
